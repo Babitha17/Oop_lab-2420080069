77875fe208d18cf4d1f07f652acc0ccf9c70f74a
Babitha17 <gandikotababitha79@gmail.com>
2025-08-09 09:34:38 +0530

    Add files via upload

---
+++ Task4/src/lab4/TestStudent.java
@@ -0,0 +1,23 @@
+package lab4;
+import java.util.Scanner;
+public class TestStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        class_student_details s1=new class_student_details();
+		        
+		        s1.setDetails(478, 9.43f, "Babitha");
+		        s1.showDetails();
+		           
+		        s1.rollno=420;
+		        s1.grade=0.23f;
+		        s1.name="Hello";
+		        s1.showDetails();
+		  }
+
+		}
+
+	}
+
+}
